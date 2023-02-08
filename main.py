4baa8e737682c3151e0bafe2dbbbbcb39c55fa47
rombimiau <roma.box.sup@gmail.com>
2023-02-08 13:57:51 +0300

    hh

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-import telebot
-
-bot = telebot.TeleBot('5873726528:AAFY5G4Z_qNnyIyBTzGZzWrKyDXY5M0m_b8')
-
-
-@bot.message_handler(commands=['start'])
-def start_message(message):
-    bot.send_message(message.chat.id,"Привет iuhiub привет ✌️ ")
-
-
-@bot.message_handler(content_types=["new_chat_members"])
-def foo(message):
-    user_name = message.new_chat_members[0].username
-    bot.reply_to(message, f"welcome @{user_name}")
-    bot.send_photo(message.chat.id, open("C://Users//Roma//Desktop//photo_2023-01-13_13-23-27 (2).jpg", 'rb'), caption='Джими рад тебя видеть')
+import telebot 
+from telebot import types 
+ 
+bot = telebot.TeleBot('5873726528:AAFY5G4Z_qNnyIyBTzGZzWrKyDXY5M0m_b8') 
+ 
+ 
+@bot.message_handler(commands=['start']) 
+def start_message(message): 
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True) 
+    btn1 = types.KeyboardButton("Я знаю, что купить") 
+    btn2 = types.KeyboardButton("я пока не знаю, что купить") 
+    markup.add(btn1, btn2) 
+    bot.send_message(message.chat.id, text="Здравствуйте! Я бот-консультант. Подскажите, знаете ли вы, что вы хотите приобрести?".format(message.from_user), reply_markup=markup) 
+ 
+ 
+@bot.message_handler(content_types=["new_chat_members"]) 
+def foo(message): 
+    user_name = message.new_chat_members[0].username 
+    bot.reply_to(message, f"welcome @{user_name}") 
+    bot.send_photo(message.chat.id, open("C://Users//Roma//Desktop//photo_2023-01-13_13-23-27 (2).jpg", 'rb'), caption='Джими рад тебя видеть') 
 bot.polling(none_stop=True)
